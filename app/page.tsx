9f608b97a60d82e16961a4bb39746be491fba3f7
gabrielubiale <gabrielfubiale@gmail.com>
2025-09-22 20:37:07 -0300

    React query configuration and useSessionResults hook

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@
 
 import Image from "next/image";
 import { seasonRaces } from '../store/useRaceStore'
+import { useSessionResults } from "@/hooks/useSessionResults";
 
 export default function Home() {
 
   const { season, setSeason, allRaces, setRaces } = seasonRaces()
+
+  const { data, isLoading, error } = useSessionResults(7782)
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
---
+++ hooks/useSessionResults.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+
+async function fetchSessionResults(sessionKey: number) {
+   const url = new URL('https://api.openf1.org/v1/session_result')
+   url.searchParams.append("session_key", String(sessionKey))
+
+   const response = await fetch(url.toString())
+
+   if(!response.ok) {
+        throw new Error('Error - session result api failed')
+   }
+   return response.json()
+}
+
+export function useSessionResults(sessionKey: number) {
+  return useQuery({
+    queryKey: ['session_results', sessionKey],
+    queryFn: () => fetchSessionResults(sessionKey),
+    enabled: !!sessionKey
+  })
+}
