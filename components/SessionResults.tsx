f783933b2422015df92e7c2a5497981174cbb6ae
gabrielubiale <gabrielfubiale@gmail.com>
2025-09-23 16:57:07 -0300

    DriverInformation hook and modal criation

--- components/SessionResults.tsx
+++ components/SessionResults.tsx
@@ -2,6 +2,13 @@
 
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 import LaunchIcon from '@mui/icons-material/Launch';
+import { useDriverInformation } from '@/hooks/useDriverInformation'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { useState } from "react";
+import { SessionsInformation } from '../store/useRaceStore'
 
 interface SessionResult {
   position: number;
@@ -17,44 +24,99 @@ interface SessionResultsProps {
   results?: SessionResult[];
 }
 
-const handleDriverNumberCLicked = (value: number) => {
-  console.log("Value: ", value)
-}
 
 export function SessionResults({ results }: SessionResultsProps) {
+
+  const [open, setOpen] = useState(false);
+  const [selectedDriver, setSelectedDriver] = useState<number | null>(null);
+  const { sessionKeySelected } = SessionsInformation()
+
+  const { data: driverInfo, isLoading, error } = useDriverInformation(selectedDriver!, sessionKeySelected);
+
+  const handleDriverNumberCLicked = (value: number) => {
+    setSelectedDriver(value);
+    setOpen(true);
+  };
 
   const lastRaceResults = results || [];
 
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="session results table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Position</TableCell>
-            <TableCell>Driver Number</TableCell>
-            <TableCell>Number of Laps</TableCell>
-            <TableCell>Duration</TableCell>
-            <TableCell>Gap to Leader</TableCell>
-            <TableCell>Points</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {lastRaceResults.map((row) => (
-            <TableRow key={row.driver_number}>
-              <TableCell>{row.position}</TableCell>
-              <TableCell className="cursor-pointer"
-                onClick={() => handleDriverNumberCLicked(row.driver_number)}>
-                {row.driver_number}
-                <LaunchIcon sx={{ marginLeft: '15px', width: '20px', height: '20px' }} />
-              </TableCell>
-              <TableCell>{row.number_of_laps}</TableCell>
-              <TableCell>{row.duration}</TableCell>
-              <TableCell>{row.gap_to_leader}</TableCell>
-              <TableCell>{row.points}</TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table aria-label="session results table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Position</TableCell>
+              <TableCell>Driver Number</TableCell>
+              <TableCell>Number of Laps</TableCell>
+              <TableCell>Duration</TableCell>
+              <TableCell>Gap to Leader</TableCell>
+              <TableCell>Points</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {lastRaceResults.map((row) => (
+              <TableRow key={row.driver_number}>
+                <TableCell>{row.position}</TableCell>
+                <TableCell className="cursor-pointer"
+                  onClick={() => handleDriverNumberCLicked(row.driver_number)}>
+                  {row.driver_number}
+                  <LaunchIcon sx={{ marginLeft: '15px', width: '20px', height: '20px' }} />
+                </TableCell>
+                <TableCell>{row.number_of_laps}</TableCell>
+                <TableCell>{row.duration}</TableCell>
+                <TableCell>{row.gap_to_leader}</TableCell>
+                <TableCell>{row.points}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {/* <Modal className="w-0.5" open={open} onClose={() => setOpen(false)}>
+        <div style={{ background: "white", padding: "20px" }}>
+          {isLoading && <p>Loading driver info...</p>}
+          {error && <p>Failed to load driver info</p>}
+          {driverInfo && (
+            <pre>{JSON.stringify(driverInfo, null, 2)}</pre>
+          )}
+        </div>
+      </Modal> */}
+
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box
+          sx={{
+            position: "absolute" as const,
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 800,
+            bgcolor: "background.paper",
+            borderRadius: 2,
+            boxShadow: 24,
+            color: '#000',
+            p: 4,
+          }}
+        >
+          <Typography variant="h6" component="h2" gutterBottom>
+            Driver Information
+          </Typography>
+
+          {isLoading && <Typography>Loading driver info...</Typography>}
+          {error && <Typography color="error">Failed to load driver info</Typography>}
+          {driverInfo && (
+            <pre style={{ fontSize: "14px", background: "#f5f5f5", padding: "10px", borderRadius: "8px" }}>
+              {JSON.stringify(driverInfo, null, 2)}
+            </pre>
+          )}
+
+          <Box mt={3} display="flex" justifyContent="flex-end">
+            <Button variant="contained" color="primary" onClick={() => setOpen(false)}>
+              Fechar
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
   );
 }
