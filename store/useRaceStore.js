1ace062a072d2c3bd0389870357b214705d5ed50
gabrielubiale <gabrielfubiale@gmail.com>
2025-10-14 14:50:52 -0300

    UseRaceStore update

--- store/useRaceStore.js
+++ store/useRaceStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 
 export const SessionsInformation = create((set) => ({
     sessionKeySelected: 'latest',
-    setSessionKeysSelected: (sessionKeySelected) => set({sessionKeySelected}),    
+    setSessionKeysSelected: (sessionKeySelected) => set({sessionKeySelected}),
 }))
 
 export const DriversInformation = create((set) => ({
